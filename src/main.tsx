621edc1b140e058837027746b391d933b21b66b0
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2023-08-18 11:54:26 +0200

    Clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
